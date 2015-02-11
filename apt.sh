84388b3e767fe4d07be634ef9209c7da91583d16
Steve Helms <shelms@dnspark.com>
2015-02-11 08:28:08 -0600

    Adding perl packages to install and enabling mcrypt in php

--- apt.sh
+++ apt.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 apt-get update
-apt-get -y install php5-mcrypt fail2ban
+apt-get -y install php5-mcrypt fail2ban libxml-treepp-perl \
+    libmath-round-perl libexcel-writer-xlsx-perl cpanminus \
+    libcrypt-ssleay-perl libjson-perl liblwp-protocol-https-perl
+
+php5enmod mcrypt
+#
+# Install a few non packaged items. Use cpanm for low memory systems
+#
+cpanm WWW::Twilio::API
+
