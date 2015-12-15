8509129841d95cb17b047c803e7816439c3875f8
Steve Helms <steve@cadenrock.com>
2015-12-15 17:39:47 -0600

    Removing php5 install and other services

--- apt.sh
+++ apt.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 apt-get update
-apt-get -y install php5-mcrypt fail2ban libxml-treepp-perl \
+apt-get -y install fail2ban libxml-treepp-perl \
     libmath-round-perl libexcel-writer-xlsx-perl cpanminus \
     libcrypt-ssleay-perl libjson-perl liblwp-protocol-https-perl
 
-php5enmod mcrypt
+# apt-get -y install php5-mcrypt phpmyadmin redis-server mongodb-server mongodb-client php5-mongo
+# php5enmod mcrypt
+
 #
 # Install a few non packaged items. Use cpanm for low memory systems
 #
